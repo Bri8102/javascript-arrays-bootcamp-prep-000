b12e4632ee6959ffd01798e6d8c76bb1eabdcc18
Briana Oniru <damioniru16@gmail.com>
2020-07-02 02:16:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,4 +4,6 @@ function addElementToBeginningOfArray(array, element) {
   var newArray = [element,...array];
   return newArray;
 }
-funtion 
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element);
+}
