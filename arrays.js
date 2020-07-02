746da94d3cb494a9763c7a1d60fe8d6972beb1c2
Briana Oniru <damioniru16@gmail.com>
2020-07-02 02:14:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  newArray = [element,...];
+  var newArray = [element,...];
   return newArray;
 }
